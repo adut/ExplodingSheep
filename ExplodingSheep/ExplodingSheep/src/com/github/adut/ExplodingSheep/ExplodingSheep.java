a488935e34081d29619efcea68b7b8cdcbd93a53
adut <geektravels@gmail.com>
2013-04-10 21:20:27 -0700

    Checking in updates addition of config file

--- ExplodingSheep/ExplodingSheep/src/com/github/adut/ExplodingSheep/ExplodingSheep.java
+++ ExplodingSheep/ExplodingSheep/src/com/github/adut/ExplodingSheep/ExplodingSheep.java
@@ -1,5 +1,41 @@
 package com.github.adut.ExplodingSheep;
 
-public class ExplodingSheep {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.permissions.*;
+
+public class ExplodingSheep extends JavaPlugin{
+	@Override
+	public void onEnable() {
+		getLogger().info("Exploding Sheep enabled");
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("Exploding Sheep disabled");
+	}
+	
+	
+	/*
+	 * On command check for the following
+	 * @see org.bukkit.plugin.java.JavaPlugin#onCommand(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
+	 */
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("xsh")){ // If the player typed /basic then do the following...
+			if (sender instanceof Player) {
+				Player player = (Player) sender;
+				// do some stuff
+			} else {
+		         sender.sendMessage("You must be a player!");
+		         return false;
+			}
+			// doSomething
+			return false;
+		} //If this has happened the function will return true. 
+	        // If this hasn't happened the a value of false will be returned.
+		return false; 
+	}
 
 }
