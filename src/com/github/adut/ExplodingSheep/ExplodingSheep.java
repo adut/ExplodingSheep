031b86c0375a2d1e499ef6863c8a17fea5589397
adut <geektravels@gmail.com>
2013-05-11 19:11:04 -0700

    Added xscolor and xsexp parameters to config file and also allow setting from console w/ commands, added config file.

--- src/com/github/adut/ExplodingSheep/ExplodingSheep.java
+++ src/com/github/adut/ExplodingSheep/ExplodingSheep.java
@@ -1,42 +1,74 @@
 package com.github.adut.ExplodingSheep;
 
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Sheep;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerShearEntityEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.configuration.*;
+import org.bukkit.configuration.file.*;
 
 
-public class ExplodingSheep extends JavaPlugin{
+
+public class ExplodingSheep extends JavaPlugin implements Listener {
+	
+
+
 	
 	@Override
 	public void onEnable() {
 		
+		
 		getLogger().info("Exploding Sheep enabled");
-		getServer().getPluginManager().registerEvents(new ShearListener(), this);
+		getServer().getPluginManager().registerEvents(this, this);
+		this.saveDefaultConfig(); // Set up the default plugin if it's not there
+		
 	}
 	
 	@Override
 	public void onDisable() {
 		getLogger().info("Exploding Sheep disabled");
+		this.saveDefaultConfig(); // Set up the default plugin if it's not there
 	}
 	
+	@EventHandler
+	public void onShear(PlayerShearEntityEvent event) {
+		Entity sheared = event.getEntity();
+		if (sheared instanceof Sheep)
+		{
+			Sheep sheep = (Sheep) sheared;
+			/* If the sheep isn't already sheared check wool color */
+			if(!sheep.isSheared()) {
+				int expStrength = this.getConfig().getInt("Global.xsexp");
+				String excolor = this.getConfig().getString("Global.xscolor");
+				if(excolor.equals("ALL") || sheep.getColor().name().equals(excolor)) {					
+					Location sheepLoc = sheared.getLocation();
+					sheared.getWorld().createExplosion(sheepLoc,expStrength);
+				}
+				
+			}
+		}
+		
+	}
+
+
 	
 	/*
 	 * On command check for the following
 	 * @see org.bukkit.plugin.java.JavaPlugin#onCommand(org.bukkit.command.CommandSender, org.bukkit.command.Command, java.lang.String, java.lang.String[])
 	 */
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
-		if(cmd.getName().equalsIgnoreCase("xsh")){ // If the player typed /basic then do the following...
-			if (sender instanceof Player) {
-				// Player player = (Player) sender;
-				// do some stuff
-			} else {
-		         sender.sendMessage("You must be a player!");
-		         return false;
-			}
-			// doSomething
-			return false;
-		} //If this has happened the function will return true. 
+		String cmdName = cmd.getName();
+		
+		if(cmdName.equals("xsexp")) {
+			this.getConfig().set("xsexp", args[0]);
+		} else if(cmdName.equals("xscolor")) {
+			this.getConfig().set("xscolor", args[0]);
+		}
 	        // If this hasn't happened the a value of false will be returned.
 		return false; 
 	}
